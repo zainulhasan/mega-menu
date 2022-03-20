91170ab15fdec49a2a05dc221a0c267d7df05694
zain <hassan9224@gmail.com>
2022-03-20 15:37:55 +0500

    inital commit

--- pages/components/menu/index.tsx
+++ pages/components/menu/index.tsx
@@ -1,12 +1,61 @@
 import styles from './menu.css';
-const Menu = function () {
 
+
+
+
+
+const Menu = function (props) {
+
+    const menuList = props.attributes.Lev0;
     return (
         <>
-            Menu
+            <div className="level-zero">
+                {menuList.map((item: any, index: number) => {
+
+
+                    const subMenu: Array<any> = [];
+
+                    Object.values(item).map((sub, index) => {
+                        if (Array.isArray(sub)) {
+                            subMenu.push(sub);
+                        }
+                    });
+
+
+                    const childList = subMenu.map((subItems, index) => {
+                        return <div key={index} className={`subgroup submenu-${index}`}>
+
+                            {subItems.map((subItem: any) => {
+
+                                return (
+                                    <div key={subItem.id} className={subItem.id}>
+                                        <a className="level-two-child" href="">{subItem.label}</a>
+                                    </div>
+                                )
+                            })
+                            }
+
+                        </div>
+                    })
+
+
+
+
+
+                    return (
+                        <div key={item.id} className="level-one">
+                            <a href="">{item.Heading}</a>
+                            <div className="level-two">
+                                {childList}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     );
 }
+
 
 
 export default Menu;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,27 @@ import Image from 'next/image'
 import Meta from './components/meta'
 import Menu from './components/menu'
 
-const Home: NextPage = () => {
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch('https://bezoge.nftendo.net/api/header-master?populate%5bLev0%5d%5bpopulate%5d%5bColumnLinks1%5d=*&populate%5bLev0%5d%5bpopulate%5d%5bColumnLinks2%5d=*&populate%5bLev0%5d%5bpopulate%5d%5bImageLink1%5d%5bpopulate%5d=*&populate%5bLev0%5d%5bpopulate%5d%5bImageLink2%5d%5bpopulate%5d=*')
+  const menuList = await res.json()
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      menuList,
+    },
+  }
+}
+
+
+
+
+
+const Home: NextPage = ({menuList}: any) => {
   return (
     <div>
 
@@ -13,12 +33,13 @@ const Home: NextPage = () => {
       />
 
       <header>
-        <Menu />
+        <Menu {...menuList.data} />
       </header>
 
 
     </div>
   )
 }
+
 
 export default Home
